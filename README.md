6dd4d6c50ff4ac5c326c0cbf3cad23b83669a83e
Dani Berentzen <nonfiction83@users.noreply.github.com>
2021-09-14 20:55:08 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# posten-delivery-checker
+
+## Introduction
+After 2020 Posten only delivers post every other day. They provide a web site, but no proper API: https://www.posten.no/levering-av-post-2020. This application aims to serve a simple web site for use on information screens to show scheduled post deliveries for a given postal code in Norway. 
+
+This app fetches the required information and serves a formatted HTML page, customized for the authors use case. Feel free to modify the output to your liking if you fork the repo.
+
+## Acknowledgment
+Special thanks to a good friend for providing most of the code and letting me tweak it further! 👏
---
+++ main.go
@@ -0,0 +1,119 @@
+package main
+
+import (
+	"embed"
+	"encoding/json"
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"time"
+)
+
+const (
+	schedule   = 15 * time.Minute
+	postalCode = "0553"
+	port       = "5055"
+)
+
+var (
+	//go:embed index.gohtml
+	res   embed.FS
+	pages = map[string]string{
+		"/": "index.gohtml",
+	}
+	client          = http.DefaultClient
+	currentResponse = new(PostalCodeLookupResponse)
+)
+
+type PostalCodeLookupResponse struct {
+	StreetAddressRequest bool     `json:"isStreetAddressReq"`
+	DeliveryDays         []string `json:"nextDeliveryDays"`
+	LastUpdated          time.Time
+}
+
+func main() {
+	kickstart := make(chan bool, 1)
+	go fetchSchedule(kickstart)
+	log.Println("kickstarting data fetching")
+	kickstart <- true
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		time.Now()
+		page, ok := pages[r.URL.Path]
+		if !ok {
+			w.WriteHeader(http.StatusNotFound)
+			return
+		}
+		tpl, err := template.ParseFS(res, page)
+		if err != nil {
+			log.Printf("page %s not found in pages cache...", r.RequestURI)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Content-Type", "text/html")
+		w.WriteHeader(http.StatusOK)
+
+		data := map[string]interface{}{
+			"deliveryDays": currentResponse.DeliveryDays,
+			"lastUpdated":  currentResponse.LastUpdated,
+			"missingData":  len(currentResponse.DeliveryDays) == 0,
+			"postalCode":   postalCode,
+		}
+
+		if err := tpl.Execute(w, data); err != nil {
+			return
+		}
+	})
+	http.FileServer(http.FS(res))
+	log.Printf("server started on port %s...", port)
+	err := http.ListenAndServe(fmt.Sprintf(":%s", port), nil)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func fetchSchedule(externalEvent chan bool) {
+	ticker := time.NewTicker(schedule)
+	log.Printf("scheduling updates at interval %+v", schedule)
+
+	for {
+		select {
+		case <-externalEvent:
+			if data, err := fetchData(); err == nil {
+				currentResponse = data
+			}
+		case <-ticker.C:
+			if data, err := fetchData(); err == nil {
+				currentResponse = data
+			}
+		}
+	}
+}
+
+func fetchData() (*PostalCodeLookupResponse, error) {
+	log.Println("fetching newest data from Posten")
+	var p PostalCodeLookupResponse
+
+	req, err := http.NewRequest("GET", fmt.Sprintf("https://www.posten.no/levering-av-post-2020/_/component/main/1/leftRegion/1?postCode=%s", postalCode), nil)
+	if err != nil {
+		log.Fatal("could not construct request, something is seriously wrong")
+	}
+
+	req.Header.Set("X-Requested-With", "XMLHttpRequest")
+	res, err := client.Do(req)
+	if err != nil {
+		log.Printf("could not fetch data from Posten: %+v", err)
+		return nil, err
+	}
+
+	err = json.NewDecoder(res.Body).Decode(&p)
+	if err != nil {
+		log.Printf("could not decode JSON from Posten: %+v", err)
+		return nil, err
+	}
+
+	log.Println("successfully fetched new data from Posten")
+	p.LastUpdated = time.Now()
+	return &p, nil
+}
